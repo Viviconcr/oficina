0137a501ed23c704f897d4a8ff2b8be2826e082e
eperazaco <eperaza@prointeccr.com>
2021-08-25 10:12:37 -0600

    CRM Vivicon Elimina View

--- crm_vivicon/models/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, _
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    group_aprobar_negociaciones = fields.Boolean(string="Puede aprobar negociaciones", implied_group='crm_vivicon.group_aprobar_negociaciones')
